2c9d7871f1bc1f6abe2e23074f6e4772f551a944
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-07-28 22:11:18 +0530

    Feat: Password hashing on signup

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user.model')
+const bcrypt = require('bcryptjs')
 
 router.route('/')
     .get((req,res)=>{
@@ -10,7 +11,10 @@ router.route('/')
 router.route('/signup')
     .post(async(req,res)=>{
         try{
-            const user = req.body;
+            const user = req.body
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(user.password,salt)
+            user.password = hashedPassword
             const newUser = new User(user)
             addedUser = await newUser.save()
             res.json({success:true,newUser})
